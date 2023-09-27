841529e694dfb8997301b4d86786d7118d57530e
chebolunaveen <88605837+chebolunaveen@users.noreply.github.com>
2023-09-27 19:36:39 +0530

    Update uploadJfrog.groovy

--- vars/uploadJfrog.groovy
+++ vars/uploadJfrog.groovy
@@ -1,3 +1,3 @@
 def call(){
-  sh 'curl -X PUT -u admin -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://<EC2IP>:8082/artifactory/example-repo-local/'
+  sh 'curl -X PUT -u admin -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://http://54.234.69.16/:8082/artifactory/example-repo-local/'
 }
